bfc15556bb3b8be4365a2a012f07cbb00d12e5f8
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-12-04 13:54:58 +0100

    Also search first name

--- AnnotationhelperPlugin.class.php
+++ AnnotationhelperPlugin.class.php
@@ -16,7 +16,7 @@ class AnnotationhelperPlugin extends StudIPPlugin implements SystemPlugin {
     public function show_action() {
         if (Request::submitted('search')) {
             $search = "%" . Request::get('search') . "%";
-            $stmt = DBManager::get()->prepare("SELECT username,CONCAT_WS(' ',vorname, nachname) as name FROM auth_user_md5 WHERE (username LIKE :search OR nachname LIKE :search ) AND " . get_vis_query() . " LIMIT 5");
+            $stmt = DBManager::get()->prepare("SELECT username,CONCAT_WS(' ',vorname, nachname) as name FROM auth_user_md5 WHERE (username LIKE :search OR nachname LIKE :search OR vorname LIKE :search ) AND " . get_vis_query() . " LIMIT 5");
             $stmt->bindParam(':search', $search);
             $stmt->execute();
             echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
